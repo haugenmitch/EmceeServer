a320f17e7de7215799f91e831dfe06261b1893cf
Mitchell Haugen <haugenmitch@gmail.com>
2020-10-04 18:33:44 -0400

    clean up Freedom, extend to 5 seconds

--- commands/freedom.py
+++ commands/freedom.py
@@ -4,25 +4,24 @@ from emcee_server import Server
 
 
 def execute(server: Server, username: str, mode: str = None, value: int = None):
-    if 'death_punishment' not in server.player_data[username] \
-            or 'end_time' not in server.player_data[username]['death_punishment'] \
-            or server.player_data[username]['death_punishment']['end_time'] is None:
-        server.tell_player('You are not imprisoned')
+    end_time = server.player_data[username]['death_punishment']['end_time'] \
+        if 'death_punishment' in server.player_data[username] and \
+           'end_time' in server.player_data[username]['death_punishment'] else None
+    if end_time is None:
+        server.tell_player(username, 'You are not imprisoned')
         return False
-    end_time = server.player_data[username]['death_punishment']['end_time']
     now = datetime.datetime.now()
-    delta0 = end_time - now
-    delta1 = delta0 - datetime.timedelta(seconds=1)
-    delta2 = delta1 - datetime.timedelta(seconds=1)
+    delta = end_time - now
     server.send_command(f'title {username} times 0 20 0')
-    server.send_command(f'title {username} subtitle {{"text":"remaining in sentence","color":"#FF0000"}}')
-    server.send_command(f'title {username} title {{"text":"{str(delta0).split(".", 2)[0]}","color":"#FF0000"}}')
-    if delta1 > datetime.timedelta(seconds=0):
-        threading.Timer(1, server.send_command, (f'title {username} title {{"text":"{str(delta1).split(".", 2)[0]}",'
-                                                 f'"color":"#FF0000"}}',)).start()
-    if delta2 > datetime.timedelta(seconds=0):
-        threading.Timer(2, server.send_command, (f'title {username} title {{"text":"{str(delta2).split(".", 2)[0]}",'
-                                                 f'"color":"#FF0000"}}',)).start()
+    subtitle = f'title {username} subtitle {{"text":"remaining in sentence","color":"#FF0000"}}'
+    timeout = datetime.timedelta(seconds=0)
+    for i in range(5):
+        if delta < timeout:
+            break
+        msg = f'title {username} title {{"text":"{str(delta).split(".", 2)[0]}","color":"#FF0000"}}'
+        threading.Timer(i, server.send_command, (subtitle, )).start()
+        threading.Timer(i, server.send_command, (msg, )).start()
+        delta -= datetime.timedelta(seconds=1)
     return True
 
 
