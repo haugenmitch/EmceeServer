a10965ba73d08c022e99eab3e4af214bd6f87a51
Mitchell Haugen <haugenmitch@gmail.com>
2020-10-04 17:42:09 -0400

    Change .seconds to .total_seconds()

--- commands/collect.py
+++ commands/collect.py
@@ -13,22 +13,25 @@ def execute(server: Server, username: str, mode: str = None, value: int = None):
         if 'last_collect' in server.player_data[username]['collect'] else None
     remainder = server.player_data[username]['collect']['remainder'] \
         if 'remainder' in server.player_data[username]['collect'] else 0
-    server.player_data[username]['collect']['last_collect'] = now
+    total_hours = remainder
     for i in range(len(log_offs)):
-        if log_offs[i] < last:
+        if last is not None and log_offs[i] < last:
             continue
-        remainder += (log_ons[i + 1] - log_offs[i]).seconds / 3600.0
-    emeralds = remainder // HOURS_PER_EMERALD
-    remainder = math.fmod(remainder, HOURS_PER_EMERALD)
+        total_hours += (log_ons[i + 1] - log_offs[i]).total_seconds() / 3600.0
+    emeralds = int(total_hours // HOURS_PER_EMERALD)
+    remainder = math.fmod(total_hours, HOURS_PER_EMERALD)
     server.player_data[username]['collect']['remainder'] = remainder
-    server.player_data[username]['collect']['last_collect'] = last
+    server.player_data[username]['collect']['last_collect'] = now
     if emeralds > 0:
+        server.tell_player(username, f'You have earned {emeralds} emerald{"" if emeralds == 1 else "s"}')
         server.send_command(f'give {username} minecraft:emerald {emeralds}')
+    else:
+        server.tell_player(username, 'You have no emeralds to collect')
     return True
 
 
 def cost(server: Server, username: str, mode: str = None, value: int = None):
-    server.tell_player(username, f'[Collect] costs {0} emeralds')
+    server.tell_player(username, f'[Collect] has no cost')
     return 0
 
 
